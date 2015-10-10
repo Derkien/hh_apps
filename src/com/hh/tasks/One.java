1c161326ac91667464462484c1eee822b160aaa2
derkien <s.val.ivanov@yandex.ru>
2015-10-10 14:26:30 +0300

    Test Commit

---
+++ src/com/hh/tasks/One.java
@@ -0,0 +1,10 @@
+package com.hh.tasks;
+
+/**
+ * Created by DERKIEN on 10.10.2015.
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.print("Hello, HH!");
+    }
+}
